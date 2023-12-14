80515846e3a56efa7f19d9dd95c3ea51a2eb2d57
袁澧林 <kobe980918@gmail.com>
2023-12-14 17:27:15 +0800

    创建-忘记密码弹窗

---
+++ src/views/login/component/forgetPassword.vue
@@ -0,0 +1,20 @@
+<template>
+	<el-dialog v-model="dialogVisible" title="忘记密码" width="400px">
+		<el-form class="login-form">
+			<el-form-item label="输入您的注册账号">
+				<el-input placeholder="请输入您的注册账号" />
+			</el-form-item>
+			<el-form-item label="输入您的个人邮箱">
+				<el-input  show-password placeholder="请输入您的个人邮箱" />
+			</el-form-item>
+	</el-dialog>
+
+</template>
+
+<script lang="ts" setup>
+	import { ref } from 'vue'
+	const dialogVisible = ref(true)
+</script>
+
+<style lang="scss" scoped>
+</style>
