5f4d26e99d791d97abcaf63da00b38ddc3666b64
袁澧林 <kobe980918@gmail.com>
2023-12-14 17:27:01 +0800

    更新 index.js

--- src/router/index.js
+++ src/router/index.js
@@ -5,8 +5,15 @@ import {
 
 // 路由配置
 const routes = [{
-	
-}]
+		path: '/',
+		redirect: '/login'
+	},
+	{
+		name: 'login',
+		path: '/login',
+		component: () => import('@/views/login/index.vue')
+	},
+]
 
 // 创建路由
 const router = createRouter({
