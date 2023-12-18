8485ec315201475ed3a87ddcfb7252c90f9f7a38
袁澧林 <kobe980918@gmail.com>
2023-12-18 17:27:11 +0800

    创建 index.vue

---
+++ src/views/menu/index.vue
@@ -0,0 +1,175 @@
+<!-- 菜单页 -->
+<template>
+	<div class="common-layout">
+		<el-container>
+			<el-aside width="200px">
+				<el-menu class="el-menu-vertical-demo" router>
+					<span class="menu-title">MIS</span>
+					<el-menu-item index="home"> <el-icon>
+							<House />
+						</el-icon>
+						<span>首页</span>
+					</el-menu-item>
+					<el-menu-item index="overview">
+						<el-icon>
+							<Document />
+						</el-icon>
+						<span>系统概览</span>
+					</el-menu-item>
+					<el-sub-menu index="manage_list">
+						<template #title>
+							<el-icon>
+								<User />
+							</el-icon>
+							<span>用户管理</span>
+						</template>
+						<el-menu-item-group title="管理员管理">
+							<el-menu-item index="product_manage">产品管理员</el-menu-item>
+							<el-menu-item index="users_manage">用户管理员</el-menu-item>
+							<el-menu-item index="message_manage">消息管理员</el-menu-item>
+						</el-menu-item-group>
+						<el-menu-item-group title="员工管理">
+							<el-menu-item index="user_list">用户列表</el-menu-item>
+						</el-menu-item-group>
+					</el-sub-menu>
+					<el-sub-menu index="product_list">
+						<template #title>
+							<el-icon>
+								<TakeawayBox />
+							</el-icon>
+							<span>产品管理</span>
+						</template>
+						<el-menu-item-group title="入库管理">
+							<el-menu-item index="product_manage_list">产品列表</el-menu-item>
+						</el-menu-item-group>
+						<el-menu-item-group title="出库管理">
+							<el-menu-item index="out_product_manage_list">出库列表</el-menu-item>
+						</el-menu-item-group>
+					</el-sub-menu>
+					<el-sub-menu index="message_manage">
+						<template #title>
+							<el-icon>
+								<ChatSquare />
+							</el-icon>
+							<span>消息管理</span>
+						</template>
+						<el-menu-item-group title="消息管理">
+							<el-menu-item index="message_list">消息列表</el-menu-item>
+						</el-menu-item-group>
+						<el-menu-item-group title="回收站">
+							<el-menu-item index="recycle">回收站</el-menu-item>
+						</el-menu-item-group>
+					</el-sub-menu>
+					<el-menu-item index="file">
+						<el-icon>
+							<icon-menu />
+						</el-icon>
+						<span>合同管理</span>
+					</el-menu-item>
+					<el-menu-item index="operation_log">
+						<el-icon>
+							<icon-menu />
+						</el-icon>
+						<span>操作日志</span>
+					</el-menu-item>
+					<el-menu-item index="login_log">
+						<el-icon>
+							<icon-menu />
+						</el-icon>
+						<span>登录日志</span>
+					</el-menu-item>
+					<el-menu-item index="set">
+						<el-icon>
+							<Tools />
+						</el-icon>
+						<span>系统设置</span>
+					</el-menu-item>
+				</el-menu>
+			</el-aside>
+			<el-container>
+				<el-header>
+					<span class="header-left-content">尊敬的xxx 欢迎您登录本系统</span>
+					<div class="header-right-content">
+						<el-badge>
+							<el-icon :size="20" class="message">
+								<Message />
+							</el-icon>
+						</el-badge>
+						<el-avatar :size="24" :src="circleUrl" />
+					</div>
+				</el-header>
+				<el-main>
+					<router-view></router-view>
+				</el-main>
+			</el-container>
+		</el-container>
+	</div>
+</template>
+
+<script lang="ts" setup>
+	import {
+		Menu as IconMenu,
+	} from '@element-plus/icons-vue'
+	import { ref, reactive, toRefs } from 'vue'
+	const state = reactive({
+		circleUrl:
+			'https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png',
+		squareUrl:
+			'https://cube.elemecdn.com/9/c2/f0ee8a3c7c9638a54940382568c9dpng.png',
+		sizeList: ['small', '', 'large'] as const,
+	})
+	const { circleUrl } = toRefs(state)
+</script>
+
+<style lang="scss" scoped>
+	// 侧边栏
+	.el-aside {
+		height: 100vh;
+		background: #2b303b;
+		width: 210px;
+		// 隐藏滚动条
+		overflow-x: hidden;
+
+		// 菜单
+		.el-menu {
+			background: #2b303b;
+			width: 210px;
+			height: 100vh;
+			border-right: 0px;
+
+			// 菜单标题
+			.menu-title {
+				display: flex;
+				justify-content: center;
+				color: #fff;
+				background: #2b303b;
+				font-size: 20px;
+				font-weight: 700;
+			}
+
+			.el-menu-item {
+
+				color: #fff;
+			}
+
+		}
+	}
+
+	// 无子菜单的
+	.el-menu-item:hover {
+		background: #006eff;
+	}
+
+	// 有子菜单的
+	:deep(.el-sub-menu__title:hover) {
+		background: #006eff;
+	}
+
+	:deep(.el-sub-menu__title) {
+		color: #fff;
+	}
+
+	:deep(.el-menu--inline) {
+		background: #2b303b;
+	}
+</style>
