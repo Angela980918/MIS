b4c3973a2a57a85338169c46ff152bcb4b0a8dbe
袁澧林 <kobe980918@gmail.com>
2023-12-22 17:27:52 +0800

    更新 forgetPassword.vue

--- src/views/login/component/forgetPassword.vue
+++ src/views/login/component/forgetPassword.vue
@@ -6,7 +6,7 @@
 				<el-input v-model="forgetFrom.account" placeholder="请输入您的注册账号" />
 			</el-form-item>
 			<el-form-item label="邮箱" prop="email">
-				<el-input v-model="forgetFrom.email" show-password placeholder="请输入您的个人邮箱" />
+				<el-input v-model="forgetFrom.email" placeholder="请输入您的个人邮箱" />
 			</el-form-item>
 		</el-form>
 
@@ -25,7 +25,7 @@
 	<el-dialog v-model="state.changePasswordDialog" title="修改密码" width="400px" align-center center>
 		<el-form :rules="rules" class="forget-form" :label-position="labelposition">
 			<el-form-item label="新密码" prop="password">
-				<el-input v-model="forgetFrom.password" placeholder="请输入您的新密码" />
+				<el-input v-model="forgetFrom.password" show-password placeholder="请输入您的新密码" />
 			</el-form-item>
 			<el-form-item label="确认新密码" prop="nextPassword">
 				<el-input v-model="forgetFrom.nextPassword" show-password placeholder="请确认您的新密码" />
@@ -46,6 +46,11 @@
 
 <script lang="ts" setup>
 	import { ref, reactive } from 'vue'
+	import {
+		verify, resetNewPassword
+	} from '@/api/login'
+	import { ElMessage } from 'element-plus'
+	import { getTip } from '@/tips'
 
 	// 表单对齐方式
 	const labelposition = ref('top')
@@ -80,18 +85,49 @@
 		nextPassword: [
 			{ required: true, message: '请确认您的新密码', trigger: 'blur' },
 		],
-
 	})
 
 	// 下一步 --> 修改密码弹窗
-	const verifyAccount = () => {
-		state.forgetPasswordDialog = false
-		state.changePasswordDialog = true
+	const verifyAccount = async () => {
+		if (!forgetFrom.email || !forgetFrom.account)
+			return ElMessage.error(getTip('CompleteInfoForm'))
+		const res = await verify(forgetFrom)
+		if (res.status == 0) {
+			ElMessage({
+				message: res.msg,
+				type: 'success',
+			})
+			// localStorage.setItem 存放到浏览器的本地存储空间
+			// sessionStorage.setItem 存放到浏览器的会话存储空间
+			localStorage.setItem('id', res.id)
+			state.forgetPasswordDialog = false
+			state.changePasswordDialog = true
+		}
+		else {
+			ElMessage.error(res.msg)
+		}
 	}
 
 	// 修改密码
-	const resetPassword = () => {
-		state.changePasswordDialog = false
+	const resetPassword = async () => {
+		if (!forgetFrom.password || !forgetFrom.nextPassword) {
+			return ElMessage.error(getTip('CompleteInfoForm'))
+		}
+		if (forgetFrom.password == forgetFrom.nextPassword) {
+			const newPassword = forgetFrom.nextPassword
+			const id = localStorage.getItem('id')
+			const res = await resetNewPassword(id, newPassword)
+			if (res.status === 0) {
+				ElMessage({
+					message: res.msg,
+					type: 'success',
+				})
+				state.changePasswordDialog = false
+			}
+			else {
+				ElMessage.error(res.msg)
+			}
+		}
 	}
 
 	// 控制弹窗 默认关闭
