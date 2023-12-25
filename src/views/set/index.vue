ec90dec59a065f494bbff79e7e6fb53c8d59c5d0
袁澧林 <kobe980918@gmail.com>
2023-12-25 17:28:40 +0800

    更新 index.vue

--- src/views/set/index.vue
+++ src/views/set/index.vue
@@ -9,13 +9,76 @@
 						<span>用户头像：</span>
 						<div class="account-info-content">
 							<el-upload class="avatar-uploader" action="http://127.0.0.1:3007/user/uploadAvatar"
-								:show-file-list="false" :on-success="handleAvatarSuccess"
-								:before-upload="beforeAvatarUpload">
-								<img v-if="imageUrl" :src="imageUrl" class="avatar" />
+								:show-file-list="false" :on-success="handleAvatarSuccess" :before-upload="beforeAvatarUpload">
+								<img v-if="userInfoStore.imageUrl" :src="userInfoStore.imageUrl" class="avatar" />
 								<el-icon v-else class="avatar-uploader-icon">
 									<Plus />
 								</el-icon>
 							</el-upload>
+						</div>
+					</div>
+
+					<div class="account-info-wrapped">
+						<span>员工账号：</span>
+						<div class="account-info-content">
+							<el-input v-model="userForm.user_id" disabled></el-input>
+						</div>
+					</div>
+
+					<div class="account-info-wrapped">
+						<span>用户账号：</span>
+						<div class="account-info-content">
+							<el-input v-model="userForm.account" disabled></el-input>
+						</div>
+					</div>
+
+					<div class="account-info-wrapped">
+						<span>用户密码：</span>
+						<div class="account-info-content">
+							<el-button type="primary">修改密码</el-button>
+						</div>
+					</div>
+
+					<div class="account-info-wrapped">
+						<span>用户姓名：</span>
+						<div class="account-info-content">
+							<el-input v-model="userForm.name"></el-input>
+						</div>
+						<el-button type="primary">保存</el-button>
+					</div>
+
+					<div class="account-info-wrapped">
+						<span>用户性别：</span>
+						<div class="account-info-content">
+							<el-select v-model="userForm.sex">
+								<el-option label="男" value="男" />
+								<el-option label="女" value="女" />
+							</el-select>
+						</div>
+						<el-button type="primary">保存</el-button>
+					</div>
+
+					<div class="account-info-wrapped">
+						<span>用户身份：</span>
+						<div class="account-info-content">
+							<el-input v-model="userForm.identity" disabled></el-input>
+						</div>
+					</div>
+
+					<div class="account-info-wrapped">
+						<span>用户部门：</span>
+						<div class="account-info-content">
+							<el-input v-model="userForm.department" disabled></el-input>
+						</div>
+					</div>
+
+					<div class="account-info-wrapped">
+						<span>用户邮箱：</span>
+						<div class="account-info-content">
+							<el-input v-model="userForm.email"></el-input>
+						</div>
+						<div class="account-save-button">
+							<el-button type="primary">保存</el-button>
 						</div>
 					</div>
 				</el-tab-pane>
@@ -45,6 +108,20 @@
 	import type {
 		UploadProps
 	} from 'element-plus'
+	import {
+		useUserInfoStore
+	} from '@/store/userInfo.js'
+	import {
+		bindAccount,
+		getUserInfo,
+		changeName,
+		changeSex,
+		changeEmail
+	} from '@/api/userInfo.js'
+
+
+	// userInfoStore
+	const userInfoStore = useUserInfoStore()
 
 	// 默认页
 	const activeName = ref('first')
@@ -59,14 +136,30 @@
 
 	const imageUrl = ref('')
 	// 头像上传成功的函数 response回应
-	const handleAvatarSuccess: UploadProps['onSuccess'] = (
-		response,
-		uploadFile
+	const handleAvatarSuccess : UploadProps['onSuccess'] = (
+		response
 	) => {
-		imageUrl.value = URL.createObjectURL(uploadFile.raw!)
+		console.log(response, uploadFile);
+		if (response.status === 0) {
+			userInfoStore.$patch({
+				imageUrl: response.url
+			})
+			ElMessage({
+				message: response.msg,
+				type: 'success',
+			});
+			// 图片 --> 绑定账号
+			(async () => {
+				const res = await bindAccount(userInfoStore.account, response.AvatarID, response.url)
+				console.log(res)
+			})()
+		} else {
+			ElMessage.error('头像更新失败失败,请重新上传!')
+		}
+		// imageUrl.value = URL.createObjectURL(uploadFile.raw!)
 	}
 	// 头像上传之前的函数
-	const beforeAvatarUpload: UploadProps['beforeUpload'] = (rawFile) => {
+	const beforeAvatarUpload : UploadProps['beforeUpload'] = (rawFile) => {
 		if (rawFile.type !== 'image/jpeg') {
 			ElMessage.error('头像必须是jpg格式!')
 			return false
@@ -76,6 +169,27 @@
 		}
 		return true
 	}
+
+	// 用户数据接口
+	interface userData {
+		account : string, // 账号
+		name : string, // 姓名
+		sex : string, // 性别
+		identity : string, // 身份
+		department : string, // 部门
+		email : string, // 邮箱
+		user_id : string, // ID
+	}
+
+	const userForm : userData = reactive({
+		account: null,
+		name: '',
+		sex: '',
+		identity: '',
+		department: '',
+		email: '',
+		user_id: ''
+	})
 </script>
 
 <style lang="scss" scoped>
@@ -120,6 +234,11 @@
 		width: 178px;
 		height: 178px;
 		display: block;
+	}
+
+	// 输入框的长度
+	:deep(.el-input) {
+		width: 240px;
 	}
 </style>
 
