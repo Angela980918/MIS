630943c5d3349f21b6b0f9daa83c9ed6517b2821
袁澧林 <kobe980918@gmail.com>
2023-12-25 17:28:38 +0800

    更新 index.vue

--- src/views/menu/index.vue
+++ src/views/menu/index.vue
@@ -96,7 +96,7 @@
 								<Message />
 							</el-icon>
 						</el-badge>
-						<el-avatar :size="24" :src="circleUrl" />
+						<el-avatar :size="24" :src="userStore.imageUrl" />
 						<el-dropdown>
 							<span class="el-dropdown-link">
 								<el-icon>
@@ -133,11 +133,17 @@
 		reactive,
 		toRefs
 	} from 'vue'
+	import {
+		useUserInfoStore
+	} from '@/store/userInfo.js'
+
+	const userStore = useUserInfoStore()
+
 	const state = reactive({
 		circleUrl: 'https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png',
 		squareUrl: 'https://cube.elemecdn.com/9/c2/f0ee8a3c7c9638a54940382568c9dpng.png',
 		sizeList: ['small', '', 'large'] as
-		const,
+			const,
 	})
 	const {
 		circleUrl
