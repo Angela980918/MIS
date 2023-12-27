146ea5ef637d383a859c1db8d404da02f0ffcce1
袁澧林 <kobe980918@gmail.com>
2023-12-27 17:21:57 +0800

    更新 index.js

--- src/tips/index.js
+++ src/tips/index.js
@@ -2,7 +2,7 @@ export const getTip = (messageKey) => {
 	const tips = {
 		// 提示信息
 		CompleteInfoForm: '请按要求填写完整!',
-		FullLoginFrom: '请输入账号和密码'
+		FullLoginFrom: '请输入账号和密码',
 	};
 	return tips[messageKey] || '未知的错误,请联系管理员'
 
