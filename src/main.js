4faafe67169f6fe23e09fcedf0cc304e5f3026f9
袁澧林 <kobe980918@gmail.com>
2024-01-18 17:23:36 +0800

    更新 main.js

--- src/main.js
+++ src/main.js
@@ -1,37 +1,35 @@
-import {
-	createApp
-} from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 // 导入路由
-import router from './router'
+import router from "./router";
 // 导入ElementPlus
-import ElementPlus from 'element-plus'
+import ElementPlus from "element-plus";
 // 导入ElementPlusIcons
-import * as ElementPlusIconsVue from '@element-plus/icons-vue'
-import 'element-plus/dist/index.css'
+import * as ElementPlusIconsVue from "@element-plus/icons-vue";
+import "element-plus/dist/index.css";
 // 导入ElementPlus国际化
-import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+import zhCn from "element-plus/dist/locale/zh-cn.mjs";
 // 导入svg图标
-import 'virtual:svg-icons-register'
+import "virtual:svg-icons-register";
 // 导入Pinia
-import pinia from '@/store'
+import pinia from "@/store";
+// 引入全局样式
+import "@/assets/css/common.scss";
 
-
-
-const app = createApp(App)
+const app = createApp(App);
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-	app.component(key, component)
+  app.component(key, component);
 }
 
 // 设置全局异常处理
-app.config.errorHandler = ((err, vm, info) => {
-	console.log(err, vm, info);
-})
+app.config.errorHandler = (err, vm, info) => {
+  console.log(err, vm, info);
+};
 
 app.use(ElementPlus, {
-	locale: zhCn,
-})
+  locale: zhCn,
+});
 
-app.use(router)
-app.use(pinia)
-app.mount('#app')
+app.use(router);
+app.use(pinia);
+app.mount("#app");
